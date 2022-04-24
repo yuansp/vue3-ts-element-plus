1c58c165340bbee5ca761965d8dbe3c33f74c847
Bee <2507060440@qq.com>
2022-04-24 18:00:06 +0800

    添加echart

---
+++ src/views/home/echarts/gauge.vue
@@ -0,0 +1,77 @@
+<template>
+  <div ref="mychart" style="width: 100%; height: 45vh"></div>
+</template>
+<script lang='ts'>
+import { defineComponent, ref, onMounted } from "vue";
+// 引入 echarts 核心模块，核心模块提供了 echarts 使用必须要的接口。
+import * as echarts from "echarts/core";
+// 引入柱状图图表，图表后缀都为 Chart
+import { GaugeChart } from "echarts/charts";
+// 引入提示框，标题，直角坐标系，数据集，内置数据转换器组件，组件后缀都为 Component
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent,
+  DatasetComponent,
+  TransformComponent,
+} from "echarts/components";
+// 标签自动布局，全局过渡动画等特性
+import { LabelLayout, UniversalTransition } from "echarts/features";
+// 引入 Canvas 渲染器，注意引入 CanvasRenderer 或者 SVGRenderer 是必须的一步
+import { CanvasRenderer } from "echarts/renderers";
+export default defineComponent({
+  setup() {
+    echarts.use([
+      TitleComponent,
+      TooltipComponent,
+      GridComponent,
+      DatasetComponent,
+      TransformComponent,
+      GaugeChart,
+      LabelLayout,
+      UniversalTransition,
+      CanvasRenderer,
+    ]);
+    const mychart = ref<HTMLElement | null>(null);
+    let num = ref(0);
+    onMounted(() => {
+      const myCharts = ref<any>();
+      myCharts.value = echarts.init(mychart.value!);
+      let data = ref<number>(0);
+      const setopinion = () => {
+        data.value = Math.ceil(Math.random() * 100);
+        myCharts.value.setOption({
+          tooltip: {
+            formatter: "{a} <br/>{b} : {c}%",
+          },
+          series: [
+            {
+              name: "Pressure",
+              type: "gauge",
+              progress: {
+                show: true,
+              },
+              detail: {
+                valueAnimation: true,
+                formatter: "{value}",
+              },
+              data: [
+                {
+                  value: data.value,
+                  name: "SCORE",
+                },
+              ],
+            },
+          ],
+        });
+      };
+      setopinion();
+     const timer= setInterval(setopinion, 3000);
+    });
+
+    return {
+      mychart,
+    };
+  },
+});
+</script>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,25 +1,61 @@
 <template>
-  <div style="width:300px;height30px">
-    <svg-icon icon-class="table"></svg-icon>
-    啊实打实
+  <div class="contents">
+    <div class="chart">
+      <el-card class="box-card">
+        <gauge-chart />
+      </el-card>
+    </div>
+    <div class="chart">
+      <el-card class="box-card">
+        <line-chart />
+      </el-card>
+    </div>
+    <div class="chart">
+      <el-card class="box-card">
+        <pictorial-bar />
+      </el-card>
+    </div>
+    <div class="chart">
+      <el-card class="box-card">
+        <radar />
+      </el-card>
+    </div>
   </div>
-  <el-button>12312</el-button>
 </template>
-
 <script lang='ts'>
-import { ElButton } from "element-plus";
-import { defineComponent } from "vue";
-
-
+import { consumers } from "stream";
+import { defineComponent, onMounted } from "vue";
+import GaugeChart from "./echarts/gauge.vue";
+import LineChart from "./echarts/line1.vue";
+import PictorialBar from "./echarts/pictorialBar.vue";
+import Radar from "./echarts/radar.vue";
 export default defineComponent({
   components: {
-    ElButton,
+    GaugeChart,
+    LineChart,
+    PictorialBar,
+    Radar,
   },
   setup() {
 
-    return {
-      
-    };
+    return {};
   },
 });
 </script>
+<style lang="scss" scoped>
+.contents {
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+  .chart {
+    float: left;
+    padding: 10px;
+    width: 45%;
+    height: 45%;
+    .box-card {
+      height: 100%;
+      width: 100%;
+    }
+  }
+}
+</style>
