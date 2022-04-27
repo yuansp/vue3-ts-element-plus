35b5d5d9d18dcdb1f67375f53ed151305b463909
Bee <2507060440@qq.com>
2022-04-27 10:52:36 +0800

    列表动画效果

--- src/views/table/child/todolist/list.vue
+++ src/views/table/child/todolist/list.vue
@@ -1,35 +1,50 @@
 <template>
-  <div>
-    <ul class="boxs">
-      <li class="lis" v-for="(items, index) in listdata" :key="index">
-        <item :listitem="items" :index="index" :deldata="deldata" :changeStatus="changeStatus" />
-      </li>
-    </ul>
+  <div class="boxs">
+    <transition-group
+      appear
+      name="animate__animated animate__bounce"
+      enter-active-class="animate__zoomIn"
+      leave-active-class="animate__zoomOutUp"
+    >
+      <item
+        class="lis"
+        v-for="(items, index) in listdata"
+        :key="items.id"
+        :listitem="items"
+        :index="index"
+        :deldata="deldata"
+        :changeStatus="changeStatus"
+      />
+    </transition-group>
   </div>
 </template>
 <script lang='ts'>
+import "animate.css";
 import { defineComponent } from "vue";
 import Item from "./item.vue";
 export default defineComponent({
   components: {
     Item,
   },
-  props: ["listdata", "changeStatus","deldata"],
+  props: ["listdata", "changeStatus", "deldata"],
 });
 </script>
 <style lang="scss" scoped>
+.animate__animated.animate__backInRight {
+  --animate-duration: 2s;
+}
 .boxs {
   width: 30%;
   padding: 0px;
-  border: 1px solid #aaa;
-  border-bottom: none;
-}
-.lis {
-  margin: 0;
-  padding: 0;
-  height: 30px;
-  line-height: 30px;
-  border-bottom: 1px solid #aaa;
-  list-style-type: none;
+  .lis {
+    margin: 0;
+    margin-bottom: 7px;
+    padding: 0;
+    height: 30px;
+    line-height: 30px;
+    border: 1px solid #aaa;
+    list-style-type: none;
+    box-shadow: 3px 3px rgba(0, 0, 0, 0.2);
+  }
 }
 </style>
