6c965798bc59d77195f21c899c762d1bcfd69a3b
Bee <2507060440@qq.com>
2022-05-16 09:28:17 +0800

    表单样式

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,8 @@ declare module '@vue/runtime-core' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCard: typeof import('element-plus/es')['ElCard']
     ElCheckTag: typeof import('element-plus/es')['ElCheckTag']
+    ElCollapse: typeof import('element-plus/es')['ElCollapse']
+    ElCollapseItem: typeof import('element-plus/es')['ElCollapseItem']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElContainer: typeof import('element-plus/es')['ElContainer']
     ElDialog: typeof import('element-plus/es')['ElDialog']
--- src/views/meus/meuchild/meus2.vue
+++ src/views/meus/meuchild/meus2.vue
@@ -1,9 +1,29 @@
 <template>
   <div class="mains">
-    <div class="rows">
-      <span>合同类型：</span> <ProjectType />
+    <el-collapse v-model="activeNames">
+      <el-collapse-item name="1">
+        <template #title>
+          <span class="headers">基本信息</span>
+        </template>
+        <div class="rows">
+          <span class="titles">合同类型：</span> <ProjectType />
+        </div>
+        <div class="rows">
+          <span class="titles">项目名称：</span>
+          <el-input type="text" disabled class="W40"></el-input>
+        </div>
+      </el-collapse-item>
+      <el-collapse-item name="2" class="aaa">
+        <template #title>
+          <span class="headers">合同文件</span>
+        </template>
+      </el-collapse-item>
+    </el-collapse>
+    <!-- <div class="rows">
+      <span class="titles">项目名称</span>
+      <el-input type="text" disabled></el-input>
     </div>
-    <el-button @click="showDialog">选择项目按钮</el-button>
+    <el-button @click="showDialog">选择项目按钮</el-button> -->
     <el-dialog v-model="dialogVisible" title="选择项目" width="50%">
       <Project :selectProject="selectProject" :closed="closed" />
     </el-dialog>
@@ -11,7 +31,7 @@
 </template>
 <script lang='ts'>
 import { defineComponent, ref } from "vue";
-import { ElDialog, ElButton, ElTable } from "element-plus";
+import { ElDialog, ElButton, ElTable, ElInput } from "element-plus";
 import Project from "@/components/project/project.vue";
 import ProjectType from "@/components/project/projectType.vue";
 export default defineComponent({
@@ -20,9 +40,11 @@ export default defineComponent({
     ProjectType,
     ElDialog,
     ElButton,
+    ElInput,
   },
   setup() {
     const dialogVisible = ref(false);
+    const activeNames = ref(["1", "2", "3"]);
     const showDialog = () => {
       dialogVisible.value = true;
     };
@@ -37,20 +59,42 @@ export default defineComponent({
       showDialog,
       closed,
       selectProject,
+      activeNames,
     };
   },
 });
 </script>
 <style lang="scss" scoped>
-.mains{
+.mains {
+  margin: 0 auto;
   width: 90%;
-  background:rgb(234, 233, 233)
-}
-.rows{
-  padding-top:10px ;
-  text-align: center;
-  ::v-deep(.el-input__inner){
-    border-radius: 5px;
+  background: rgb(234, 233, 233);
+  .headers {
+    padding-left: 20px;
+    font-size: 25px;
+    font-weight: bold;
+    color: #41578a;
   }
+  .rows {
+    padding-top: 10px;
+    padding-left: 20px;
+    text-align: left;
+    ::v-deep(.el-input__inner) {
+      border-radius: 5px;
+    }
+    .titles {
+      padding-right: 10px;
+      font-weight: bold;
+    }
+  }
+
+  .el-collapse {
+    ::v-deep(.el-collapse-item__header) {
+      background: #eceef3;
+    }
+  }
+}
+.W40{
+  width: 40%;
 }
 </style>
