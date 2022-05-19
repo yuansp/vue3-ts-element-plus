e41ea6f24d921f389cccf4c4d0ed91eff2557007
mac <2507060440@qq.com>
2022-05-19 22:21:08 +0800

    scrollIntoView进行锚点跳转

--- src/views/meus/meuchild/meus2.vue
+++ src/views/meus/meuchild/meus2.vue
@@ -2,7 +2,7 @@
   <div class="mains">
     <el-form :model="formData">
       <el-collapse v-model="activeNames">
-        <el-collapse-item name="1">
+        <el-collapse-item name="1" class="aaa">
           <template #title>
             <span class="headers">基本信息</span>
           </template>
@@ -24,18 +24,85 @@
             <span class="headers">合同文件</span>
           </template>
         </el-collapse-item>
+        <el-collapse-item name="3" class="aaa">
+          <template #title>
+            <span class="headers">222222</span>
+          </template>
+          <div>333333333</div>
+          <div>333333333</div>
+          <div>22222222</div>
+          <div>333333333</div>
+          <div>333333333</div>
+          <div>333333333</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+        </el-collapse-item>
+        <el-collapse-item name="4" class="aaa">
+          <template #title>
+            <span class="headers">3333333</span>
+          </template>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+        </el-collapse-item>
+        <el-collapse-item name="5" class="aaa">
+          <template #title>
+            <span class="headers">4444444</span>
+          </template>
+          <div>12312312</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+        </el-collapse-item>
+        <el-collapse-item name="6" class="aaa">
+          <template #title>
+            <span class="headers">5555555</span>
+          </template>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+        </el-collapse-item>
+        <el-collapse-item name="7" class="aaa">
+          <template #title>
+            <span class="headers">666666</span>
+          </template>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+          <div>222222222</div>
+        </el-collapse-item>
       </el-collapse>
     </el-form>
+
+
+    <!-- 选择项目弹框 -->
     <el-dialog v-model="dialogVisible" title="选择项目" width="50%">
       <Project :selectProject="selectProject" :closed="closed" />
     </el-dialog>
-  </div>
-  <div style="height: 300px" class="steps">
-    <el-steps direction="vertical" :active="1">
-      <el-step title="Step 1" />
-      <el-step title="Step 2" />
-      <el-step title="Step 3" />
-    </el-steps>
+
+    <!-- 快捷导航 -->
+    <div style="height: 300px" class="steps">
+      <el-steps direction="vertical" space="10%" :active="1">
+        <el-step title="Step 1" @click="steps('1')" />
+        <el-step title="Step 2" @click="steps('2')" />
+        <el-step title="Step 3" @click="steps('3')" />
+      </el-steps>
+    </div>
   </div>
 </template>
 <script lang='ts'>
@@ -55,7 +122,7 @@ export default defineComponent({
   },
   setup() {
     const dialogVisible = ref(false);
-    const activeNames = ref(["1", "2", "3"]);
+    const activeNames = ref(["1", "2", "3", '4', '5', '6', '7']);
     const formData = reactive({
       projectType: "",
       projectTypeId: 0,
@@ -72,13 +139,17 @@ export default defineComponent({
       //名称
       formData.projectName = data.name;
       formData.projectId = data.id;
-      console.log(data);
     };
     const selectType = (data: projectType) => {
       //类型
       formData.projectType = data.type;
       formData.projectTypeId = data.id;
     };
+    const steps = (val: string) => {
+      const num = parseInt(val)
+      const elements = document.querySelectorAll('.aaa')
+      elements[num].scrollIntoView()
+    }
     return {
       activeNames,
       dialogVisible,
@@ -87,14 +158,16 @@ export default defineComponent({
       selectProject,
       showProject,
       selectType,
+      steps
     };
   },
 });
 </script>
 <style lang="scss" scoped>
 .mains {
+  position: relative;
   margin: 0 auto;
-  width: 90%;
+  width: 70%;
   background: rgb(234, 233, 233);
 
   .headers {
@@ -130,6 +203,9 @@ export default defineComponent({
   }
 
   .steps {
+    position: absolute;
+    top: 0;
+    right: -210px;
     width: 200px;
     background: #ff0;
   }
